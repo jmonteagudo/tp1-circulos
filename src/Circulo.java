e7ca72f2ee4452b35d1649e79e18c03b3bdd81d5
sbogado <seebogado@hotmail.com>
2016-05-09 11:01:50 -0300

    Prueba en clase con alumna que me interrumpiò para atender el telefono.

---
+++ src/Circulo.java
@@ -0,0 +1,16 @@
+
+public class Circulo {
+	private Double radio;
+
+	public Double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(Double radio) {
+		if(radio > 0){
+			this.radio = radio;
+		}
+	}
+	
+
+}
